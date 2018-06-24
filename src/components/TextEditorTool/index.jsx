e5f04c0bc01f81729ed9de01942d60ecd3766233
Raghavendra Sao <saoraghavendra@gmail.com>
2018-06-24 12:08:38 +0530

    done fontSize in textEditorTool

--- src/components/TextEditorTool/index.jsx
+++ src/components/TextEditorTool/index.jsx
@@ -34,67 +34,75 @@ class TextEditorTool extends Component {
         className={`col-9 sticker__editor__tool ${activeType !== undefined ? 'active' : ''}`}
         onClick={e => this.preventPropagation(e)}
       >
-        <div className={`text__editor__tool ${activeType === 'text' ? 'active' : ''}`}>
-          <input
-            id="text__bold"
-            checked={activeStyle.fontWeight === 'bold'}
-            type="checkbox"
-            name="fontWeight"
-            value={activeStyle.fontWeight === 'bold' ? 'normal' : 'bold'}
-            onChange={e => this.onTextStyleChange(e)}
-          />
-          <label htmlFor="text__bold">
-            <i className="fa fa-bold" />
-          </label>
+        {activeType === 'text' && (
+          <div className={`text__editor__tool ${activeType === 'text' ? 'active' : ''}`}>
+            <select name="fontSize" onChange={this.onTextStyleChange} value={activeStyle.fontSize}>
+              <option>Select</option>
+              {[...Array(20)].map((x, i) => {
+                return <option key={i} value={`${i + 10}px`}>{`${i + 10}px`}</option>
+              })}
+            </select>
+            <input
+              id="text__bold"
+              checked={activeStyle.fontSize === 'bold'}
+              type="checkbox"
+              name="fontWeight"
+              value={activeStyle.fontWeight === 'bold' ? 'normal' : 'bold'}
+              onChange={e => this.onTextStyleChange(e)}
+            />
+            <label htmlFor="text__bold">
+              <i className="fa fa-bold" />
+            </label>
 
-          <input
-            id="text__italic"
-            checked={activeStyle.fontStyle === 'italic'}
-            type="checkbox"
-            name="fontStyle"
-            value={activeStyle.fontStyle === 'italic' ? 'normal' : 'italic'}
-            onChange={e => this.onTextStyleChange(e)}
-          />
-          <label htmlFor="text__italic">
-            <i className="fa fa-italic" />
-          </label>
+            <input
+              id="text__italic"
+              checked={activeStyle.fontStyle === 'italic'}
+              type="checkbox"
+              name="fontStyle"
+              value={activeStyle.fontStyle === 'italic' ? 'normal' : 'italic'}
+              onChange={e => this.onTextStyleChange(e)}
+            />
+            <label htmlFor="text__italic">
+              <i className="fa fa-italic" />
+            </label>
 
-          <input
-            id="text__left"
-            checked={activeStyle.textAlign === 'left'}
-            type="radio"
-            name="textAlign"
-            value="left"
-            onChange={e => this.onTextStyleChange(e)}
-          />
-          <label htmlFor="text__left">
-            <i className="fa fa-align-left" />
-          </label>
+            <input
+              id="text__left"
+              checked={activeStyle.textAlign === 'left'}
+              type="radio"
+              name="textAlign"
+              value="left"
+              onChange={e => this.onTextStyleChange(e)}
+            />
+            <label htmlFor="text__left">
+              <i className="fa fa-align-left" />
+            </label>
 
-          <input
-            id="text__middle"
-            checked={activeStyle.textAlign === 'center'}
-            type="radio"
-            name="textAlign"
-            value="center"
-            onChange={e => this.onTextStyleChange(e)}
-          />
-          <label htmlFor="text__middle">
-            <i className="fa fa-align-center" />
-          </label>
+            <input
+              id="text__middle"
+              checked={activeStyle.textAlign === 'center'}
+              type="radio"
+              name="textAlign"
+              value="center"
+              onChange={e => this.onTextStyleChange(e)}
+            />
+            <label htmlFor="text__middle">
+              <i className="fa fa-align-center" />
+            </label>
 
-          <input
-            id="text__right"
-            checked={activeStyle.textAlign === 'right'}
-            type="radio"
-            name="textAlign"
-            value="right"
-            onChange={e => this.onTextStyleChange(e)}
-          />
-          <label htmlFor="text__right">
-            <i className="fa fa-align-right" />
-          </label>
-        </div>
+            <input
+              id="text__right"
+              checked={activeStyle.textAlign === 'right'}
+              type="radio"
+              name="textAlign"
+              value="right"
+              onChange={e => this.onTextStyleChange(e)}
+            />
+            <label htmlFor="text__right">
+              <i className="fa fa-align-right" />
+            </label>
+          </div>
+        )}
         <div className={`inline common__editor__tool ${activeType !== undefined ? 'active' : ''}`}>
           <label className="btn" onClick={this.deleteSticker}>
             <i className="fa fa-trash" />
