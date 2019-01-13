828355156c98d7315473b187af8926dc36b64083
rishabhdev <rishabh@rippling.com>
2019-01-13 20:33:42 +0530

    auth

--- src/APIS/apiConstants.js
+++ src/APIS/apiConstants.js
@@ -1,2 +1,4 @@
 export const authApi = 'https://eve123.herokuapp.com/api/auth';
 
+export const googleClientID = '1035573275457-n8745o5q4bura5nh7vj2qs1rvjb9sbc8.apps.googleusercontent.com';
+export const clientSecretKey = 'dU3j2XAopG8aaAuis3xfxvYP';
--- src/APIS/authApis.js
+++ src/APIS/authApis.js
@@ -2,7 +2,7 @@ import request from './request';
 import {authApi} from './apiConstants';
 
 export const loginUser = (provider) => {
-  const url = `${authApi}/${provider}/login`;
+  const url = `${window.location.origin}/${provider}/login`;
   return request({
     method: 'get',
     url,
