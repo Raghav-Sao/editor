00b4d45c6bdf47c33b3cb3d444e37bdb8dcd87a5
Raghav <saoraghavendra@gmail.com>
2019-08-26 23:15:01 +0530

    constant file name changes

---
+++ src/constantss/general.js
@@ -0,0 +1,16 @@
+export const general = {
+    english: {
+        HOME: 'HOME',
+        IMAGE: 'IMAGE',
+        TEXT: 'TEXT',
+        TEMPLATES: 'TEMPLATES',
+    },
+    hindi: {
+        HOME: 'HOME',
+        IMAGE: 'IMAGE',
+        TEXT: 'TEXT',
+        TEMPLATES: 'TEMPLATES',
+    },
+};
+
+export const language = 'english';
---
+++ src/constantss/textConstants.js
@@ -0,0 +1,3 @@
+export const IMAGE = 'IMAGE';
+export const TEXT = 'TEXT';
+export const SVG = 'SVG';
